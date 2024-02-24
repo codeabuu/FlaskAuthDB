d51a47bc99387fff4dd0dbe7a04d858d3bcbc869
codeabuu <abdulngui718@gmail.com>
2024-02-24 18:05:13 -0500

    updates

--- website/static/index.js
+++ website/static/index.js
@@ -1,1 +1,9 @@
-function deleteNote
+function deleteNote(nodeId) {
+  fetch('/delete-note', {
+    method: 'POST',
+    body: JSON.stringify({nodeId: nodeId}),
+  }).then((_res) => {
+    window.location.href = "/";
+  });
+
+}
